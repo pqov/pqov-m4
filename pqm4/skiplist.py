bc0318981b2e1b71c37d4779a58f1d82844f7bfc
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-01-07 13:18:53 +0800

    update skiplist

--- pqm4/skiplist.py
+++ pqm4/skiplist.py
@@ -1,2 +1,9 @@
 skip_list = [
+    # These do not work on the Nucleo, but they work in qemu
+    # Determining actual memory usage is currently not possible automatically
+    {'scheme': 'ov-Is', 'implementation':'ref', 'estmemory': 1048576},
+    {'scheme': 'ov-Is-pkc', 'implementation':'ref', 'estmemory': 1048576},
+    {'scheme': 'ov-Is-pkc-aes4', 'implementation':'ref', 'estmemory': 1048576},
+    {'scheme': 'ov-Is-pkc-skc', 'implementation':'ref', 'estmemory': 1048576},
+    {'scheme': 'ov-Is-pkc-skc-aes4', 'implementation':'ref', 'estmemory': 1048576},
 ]
